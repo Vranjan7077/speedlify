a8a200200cdf819efbf56f355998788ba63aefa9
vranjan <ranvinay123@gmail.com>
2022-05-07 20:19:59 +0530

    :tada: Inital setup

---
+++ _data/sites/blog.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: "Blog",
+  description: "Personal Blog",
+  options: {
+    frequency: 60 * 24 * 13,
+    freshChrome: "site",
+  },
+  urls: [
+    "https://blog-vranjan.netlify.app/",
+    "https://blog-vranjan.netlify.app/about/",
+    "https://blog-vranjan.netlify.app/contact/",
+  ],
+  skip: false,
+};
---
+++ _data/sites/vranjan.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: "vranjan.dev",
+  description: "Personal Website",
+  options: {
+    frequency: 1,
+    freshChrome: "site",
+  },
+  urls: [
+    "https://vranjan.dev/",
+    "https://vranjan.dev/work/",
+    "https://vranjan.dev/blog/",
+    "https://vranjan.dev/contact/",
+  ],
+  skip: false,
+};
